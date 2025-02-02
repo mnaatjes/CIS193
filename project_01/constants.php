a0731236c1d0a1ad72cb6c10ed50296ca2540951
mnaatjes <michael.naatjes87@gmail.com>
2025-02-02 15:08:35 -0500

    entered and tested table component

---
+++ project_01/constants.php
@@ -0,0 +1,16 @@
+<?php
+    /**
+     * const DIR__SRC
+     */
+    define('DIR__SRC', '../../../src');
+    /**
+     * const DIR__UTILS
+     * utils directory main
+     */
+    define('DIR__UTILS', DIR__SRC . '/shared-library/php/utils/utils-main.php');
+    /**
+     * const DIR__COMPONENTS
+     * HTML Components Library
+     */
+    define('DIR__COMP', DIR__SRC . '/shared-library/php/components/components.php');
+?>
--- project_01/index.php
+++ project_01/index.php
@@ -1,7 +1,44 @@
 <?php
     /**
-     * Requires
-     * Commit after adding src folder to workspace
+     * Require
+     * const    constants
+     * dir      src directory
      */
-    echo 'Badger, badger, badger, barger... mushroom! mushroom!';
+    require_once 'constants.php';
+    require_once DIR__UTILS;
+    require_once DIR__COMP;
+    /**
+     * turn errors on
+     */
+    enableErrors();
+    /**
+     * build personal information component
+     */
+    $personalInfo   = file_get_contents('data/info.json', true);
+    $infoData       = [
+        [
+            'name' => 'John Doe',
+            'phone' => '555-123-4567',
+            'fax' => '555-987-6543',
+            'email' => 'john.doe@example.com'
+        ],
+        [
+            'name' => 'Jane Smith',
+            'phone' => '555-987-6543',
+            'fax' => '555-123-4567',
+            'email' => 'jane.smith@example.com'
+        ],
+        [
+            'name' => 'David Lee',
+            'phone' => '555-555-5555',
+            'fax' => '555-555-5555',
+            'email' => 'david.lee@example.com'
+        ]
+    ];
+    $info = new HTMLComponent('div', [], [
+        new HTMLComponent('h1', [], [], ['textContent'=>'Personal Information']),
+        new TableComponent('Info', $infoData)
+    ], []);
+    $info->render();
+    $info->mount();
 ?>
