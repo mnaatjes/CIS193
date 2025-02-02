d8504f5a6f1c505fa1d5ec2680a3c1590cb5c32b
mnaatjes <michael.naatjes87@gmail.com>
2025-02-02 12:24:08 -0500

    added lorem ipsum

--- project_01/index.php
+++ project_01/index.php
@@ -1,5 +1,17 @@
 <?php
     /**
      * Requires
+     * 1. Created CIS193 Repo
+     * 2. Created test_repo
+     * 3. Add test_repo to workspace
+     *      a. add folder test_repo to wrokspace
+     *      b. test_repo added to workspace
+     *      c. 2 git repos
+     * 4. test commit to test_repo to make sure they stay seperate
+     * 
      */
+    
+    echo '<p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint aut ad quidem labore repudiandae cum reiciendis reprehenderit, at cupiditate blanditiis vitae doloremque quasi doloribus sed optio, ipsum, tempore vel voluptatibus.
+    </p>';
 ?>
