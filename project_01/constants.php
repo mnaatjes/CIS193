90083aac0e3f839602fa85502c9d9a7d32ae21a2
mnaatjes <michael.naatjes87@gmail.com>
2025-02-20 16:56:47 -0500

    added gitignore

--- project_01/constants.php
+++ project_01/constants.php
@@ -2,15 +2,18 @@
     /**
      * const DIR__SRC
      */
-    define('DIR__SRC', '../../../src');
+    //define('DIR__SRC', '../../../src');
+    define('DIR__SRC', './src');
     /**
      * const DIR__UTILS
      * utils directory main
      */
-    define('DIR__UTILS', DIR__SRC . '/shared-library/php/utils/utils-main.php');
+    //define('DIR__UTILS', DIR__SRC . '/shared-library/php/utils/utils-main.php');
+    define('DIR__UTILS', DIR__SRC . '/utils/utils-main.php');
     /**
      * const DIR__COMPONENTS
      * HTML Components Library
      */
-    define('DIR__COMP', DIR__SRC . '/shared-library/php/components/components.php');
+    //define('DIR__COMP', DIR__SRC . '/shared-library/php/components/components.php');
+    define('DIR__COMP', DIR__SRC . '/components/components.php');
 ?>
