0b54976c0dfaf7ef454c03a8ed374453bd8aaa03
mnaatjes <michael.naatjes87@gmail.com>
2025-04-25 20:51:10 -0400

    Started Project 04 CIS 193

---
+++ project_04/index.php
@@ -0,0 +1,16 @@
+<?php
+    /**
+     * @author Michael Naatjes
+     * @since 04/24/25
+     * @package CIS 193 Project 04
+     */
+    /**
+     * Include utilities
+     */
+    
+    /**
+     * Include Frameworks:
+     * - HTTP Manager
+     * - DB Manager
+     */
+?>
